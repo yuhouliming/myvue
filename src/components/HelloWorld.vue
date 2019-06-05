2939221531d9168e6384c6fc051615a2225d4bce
wuleiming <822507481@qq.com>
2019-06-05 10:06:37 +0800

    提交代码

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,113 +1,101 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+    <Layout>
+      <Header>
+        <Menu mode="horizontal" theme="dark" active-name="1">
+          <div class="layout-logo"></div>
+          <div class="layout-nav">
+            <MenuItem name="1">
+              <Icon type="ios-navigate"></Icon>
+              头条
+            </MenuItem>
+            <MenuItem name="2">
+              <Icon type="ios-keypad"></Icon>
+              腾讯
+            </MenuItem>
+            <MenuItem name="3">
+              <Icon type="ios-analytics"></Icon>
+              网易
+            </MenuItem>
+            <MenuItem name="4">
+              <Icon type="ios-paper"></Icon>
+              新浪
+            </MenuItem>
+          </div>
+        </Menu>
+      </Header>
+      <Layout>
+        <Sider hide-trigger :style="{background: '#fff'}">
+          <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
+            <Submenu name="1">
+              <template slot="title">
+                <Icon type="ios-navigate"></Icon>
+                科技
+              </template>
+              <MenuItem name="1-1">华为</MenuItem>
+              <MenuItem name="1-2">腾讯</MenuItem>
+              <MenuItem name="1-3">阿里</MenuItem>
+            </Submenu>
+            <Submenu name="2">
+              <template slot="title">
+                <Icon type="ios-keypad"></Icon>
+                数码
+              </template>
+              <MenuItem name="2-1">苹果</MenuItem>
+              <MenuItem name="2-2">诺基亚</MenuItem>
+            </Submenu>
+            <Submenu name="3">
+              <template slot="title">
+                <Icon type="ios-analytics"></Icon>
+                小说
+              </template>
+              <MenuItem name="3-1">现代小说</MenuItem>
+              <MenuItem name="3-2">玄幻小说</MenuItem>
+            </Submenu>
+          </Menu>
+        </Sider>
+        <Layout :style="{padding: '0 24px 24px'}">
+          <Breadcrumb :style="{margin: '24px 0'}">
+            <BreadcrumbItem>Home</BreadcrumbItem>
+            <BreadcrumbItem>Components</BreadcrumbItem>
+            <BreadcrumbItem>Layout</BreadcrumbItem>
+          </Breadcrumb>
+          <Content :style="{padding: '24px', minHeight: '280px', background: '#fff'}">
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+  export default {
+
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .layout{
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+  }
+  .layout-logo{
+    width: 100px;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    float: left;
+    position: relative;
+    top: 15px;
+    left: 20px;
+  }
+  .layout-nav{
+    width: 420px;
+    margin: 0 auto;
+    margin-right: 20px;
+  }
 </style>
---
+++ src/components/test.vue
@@ -0,0 +1,43 @@
+
+<script>
+    export default {
+      el:'test',
+      data () {
+        return {
+          msg: 'Welcome to Your Vue.js App',
+          seen:true,
+          url:'www.baidu.com',
+          input:"",
+          message:'wlm'
+        }
+      },
+      methods:{
+        reverseMessage:function () {
+            this.message=this.message.split('').reverse().join('')
+        }
+      }
+    }
+</script>
+
+
+<template>
+  <div>
+    <p>{{msg}}</p>
+    <div v-html="msg"></div>
+    <p v-if="seen">我看到你了</p>
+    <div v-if="seen">
+        if话语
+    </div>
+    <div id="test">
+      <pre><a v-bind:href="url">百度</a></pre>
+    </div>
+    <!--->
+    <input v-model="input"/>
+    <p>{{input}}</p>
+
+    <Button v-on:click = "reverseMessage" type="primary">字符串反转</Button>
+    <p>{{message}}</p>
+
+  </div>
+</template>
+
